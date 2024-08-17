b14a62a3d77dcf094446fcb429562d7212e19e0b
Md Adil <md-adil@live.com>
2024-08-17 16:28:38 +0530

    added deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  distDir: "out", // Where to export all pages
+  assetPrefix: "/spedit",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
