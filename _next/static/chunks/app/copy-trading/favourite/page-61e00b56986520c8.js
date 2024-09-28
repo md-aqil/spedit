29ccf2de806e2f0c9a32eb109b46ad8d6485e1f1
md-aqil <aqilali381@gmail.com>
2024-09-28 06:56:15 +0530

    Updates

--- _next/static/chunks/app/copy-trading/favourite/page-61e00b56986520c8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2293],{},function(n){n.O(0,[8173,1704,231,7036,2971,7023,1744],function(){return n(n.s=37036)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/copy-trading/favourite/page-df2377892d807b00.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2293],{},function(n){n.O(0,[881,8173,231,933,2971,7023,1744],function(){return n(n.s=70933)}),_N_E=n.O()}]);
